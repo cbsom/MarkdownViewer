b9e5d3c11667844727130f47315786493a0bc29c
Chaim B. Sommers <cbsommers@gmail.com>
2016-12-25 19:06:03 +0200

    Css changes to make tabs look a bit more decent.

--- MarkdownViewer/frmMain.Designer.cs
+++ MarkdownViewer/frmMain.Designer.cs
@@ -34,11 +34,12 @@
             // frmMain
             // 
             this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1204, 730);
+            this.ClientSize = new System.Drawing.Size(903, 593);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MinimumSize = new System.Drawing.Size(800, 0);
             this.Name = "frmMain";
             this.Text = "Markdown Viewer";
             this.ResumeLayout(false);
