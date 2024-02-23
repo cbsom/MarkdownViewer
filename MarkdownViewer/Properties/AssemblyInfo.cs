e03d53e1bc82243a8d3affa37a2ca94b38710682
Chaim B. Sommers <cb@charity-choice.org>
2024-02-23 10:16:28 +0200

    Version 1.2.5.1. Updated to .net verssion 4.8

--- MarkdownViewer/Properties/AssemblyInfo.cs
+++ MarkdownViewer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Compute Software Solutions")]
 [assembly: AssemblyProduct("Markdown Viewer")]
-[assembly: AssemblyCopyright("Copyright © Compute Software Solutions 2017")]
+[assembly: AssemblyCopyright("Copyright © Compute Software Solutions 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.4.2")]
-[assembly: AssemblyFileVersion("1.2.4.2")]
+[assembly: AssemblyVersion("1.2.5.1")]
+[assembly: AssemblyFileVersion("1.2.5.1")]
